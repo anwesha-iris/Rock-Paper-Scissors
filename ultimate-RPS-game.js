3e71440790f25769be8300f91553f61b812fb61f
anwesha-iris <anwesha121das@gmail.com>
2026-09-16 11:44:39 +0530

    Add files via upload

---
+++ ultimate-RPS-game.js
@@ -0,0 +1,112 @@
+let score = JSON.parse(localStorage.getItem('score'))||{
+		wins : 0,
+		losses : 0,
+		ties : 0
+	};
+
+updateScoreElement();
+
+console.log(localStorage.getItem('score'));
+
+function updateScoreElement() {
+	document.querySelector('.js-score')
+		.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+	const randomNumber=Math.random();
+	let computerMove;
+
+	if(randomNumber>=0 && randomNumber<=1/3){
+		computerMove='rock';
+	}else if(randomNumber>=1/3 && randomNumber<=2/3){
+		computerMove='paper';
+	}else if(randomNumber>=2/3 && randomNumber<=1){
+		computerMove='scissors';
+	}
+	return computerMove;
+}
+
+
+let isAutoPlaying = false;
+let intervalID;
+function autoPlay(){
+	if(!isAutoPlaying){
+		intervalID = setInterval(function(){
+			const playerMove = pickComputerMove();
+			playGame(playerMove);
+		}, 2000);
+		isAutoPlaying = true;
+	}else{
+		clearInterval(intervalID);
+		isAutoPlaying = false;
+	}
+
+}
+
+
+//playing using keyboard
+document.body.addEventListener('keydown', (event) => {
+	if(event.key === 'r'){
+		playGame('rock');
+	}else if(event.key === 'p'){
+		playGame('paper');
+	}else if(event.key === 's'){
+		playGame('scissors');
+	}
+});
+
+
+function playGame(playerMove){
+	//1.
+	const computerMove=pickComputerMove();
+
+	let result='';
+	
+	//2.
+	if(playerMove==='rock'){
+		if(computerMove==='rock'){
+			result='Tie.'
+		}else if(computerMove==='paper'){
+			result='You lose.';
+		}else if(computerMove==='scissors'){
+			result='You win.';
+		}
+	}else if(playerMove==='paper'){
+		if(computerMove==='paper'){
+			result='Tie.'
+		}else if(computerMove==='scissors'){
+			result='You lose.';
+		}else if(computerMove==='rock'){
+			result='You win.';
+		}
+	}else if(playerMove==='scissors'){
+		if(computerMove==='scissors'){
+			result='Tie.'
+		}else if(computerMove==='rock'){
+			result='You lose.';
+		}else if(computerMove==='paper'){
+			result='You win.';
+		}
+	}
+
+	//3.
+	if(result === 'You win.'){
+		score.wins += 1;
+	}else if(result === 'You lose.'){
+		score.losses += 1;
+	}else if(result === 'Tie.'){
+		score.ties += 1;
+	}
+
+	//storing the values to make the variables permanent
+	localStorage.setItem('score', JSON.stringify(score));
+
+	updateScoreElement();
+
+	document.querySelector('.js-result').innerHTML = result;
+	document.querySelector('.js-moves').innerHTML = `You picked <img src="${playerMove}-emoji.png" class="move-icon">. Computer picked <img src="${computerMove}-emoji.png" class="move-icon">.`;
+
+	
+
+}
